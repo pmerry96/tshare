d10e1c5224ea57b04c8cb1c19cc2a2067a2cdfd3
Pmerry96 <Pmerry@clemson.edu>
2020-11-10 13:23:40 -0500

    Added functionality to edit /etc/security/limits.conf automatically

--- allot_resources.sh
+++ allot_resources.sh
@@ -2,7 +2,7 @@
 # argument 1 - The user to limit - This is the username (as recognized by the OS) of a user who is
 #	to be limited in the scope of their running processes
 # argument 2 - The limiting Factor - This is the factor by which the user will be limited. They will
-# recieve 1/x(available resources), where X is the avialable resources
+# recieve 1/x(available resources), where X is the available resources
 
 #! /bin/bash/
 
@@ -12,7 +12,31 @@ if [ $? -eq 0 ]; then
 	echo "User has been found"
 	if [ $2 -gt 0 ]; then
 		echo "You have selected a limiting factor of $2"
-		./do_cgroups
+		memtot=$(awk '/MemTotal/ {printf "%.3f \n", $2/1024/1024}' /proc/meminfo)
+		memAva=$(awk '/MemAvailable/ {printf "%.3f \n", $2/1024/1024}' /proc/meminfo)
+		memact=$(awk '/Active:/ {printf "%.3f \n", $2/1024/1024}' /proc/meminfo)
+		echo Total Memory = $memtot 
+		echo Available Memory = $memAva
+		echo Active Memory = $memact
+		echo $memtot - $memAva | bc -l
+		mempctAct=$(echo $memact / $memtot | bc -l)
+		memforty=$(echo $memtot \* 40 / 100 | bc -l)
+		if [ $(echo $mempctAct '<' $memforty | bc -l) ]; then
+			#echo "beat"
+			#echo "beat"
+			memAva=$(echo " $memtot * 60 / 100" | bc -l)
+			echo $memAva
+			usermem=$(echo "$memAva/$2*1000000" | bc -l)
+			userline=$(cat /etc/security/limits.conf | grep -n $1)
+			roundedmem=$(printf "%.0f\n" $usermem)
+			echo roundedmem = $roundedmem
+			userreplace=$(echo "@$1 hard as $roundedmem")
+			echo Usermem = $userreplace
+			#first, remove any lines changing the AS limit
+			echo userline = $userline
+			#echo beat
+			echo "$userreplace" >> /etc/security/limits.conf
+		fi
 		exit
 	else
 		echo "Cannot allot resources to user with limiting factor of $2"
