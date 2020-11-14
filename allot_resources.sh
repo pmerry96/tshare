5a26afcbf3cdcc8a2f72f58102588633d3e8a9db
Pmerry96 <Pmerry@clemson.edu>
2020-11-14 15:02:58 -0500

    added logic to force a logout in 5 minutes if the user's resources changed

--- allot_resources.sh
+++ allot_resources.sh
@@ -9,58 +9,42 @@
 user=$(getent passwd | grep $1)
 
 if [ $? -eq 0 ]; then
-	echo "User has been found"
+	#echo "User has been found"
 	if [ $2 -gt 0 ]; then
-		echo "You have selected a limiting factor of $2"
+		#echo "You have selected a limiting factor of $2"
 		memtot=$(awk '/MemTotal/ {printf "%.3f \n", $2/1024/1024}' /proc/meminfo)
 		memAva=$(awk '/MemAvailable/ {printf "%.3f \n", $2/1024/1024}' /proc/meminfo)
 		memact=$(awk '/Active:/ {printf "%.3f \n", $2/1024/1024}' /proc/meminfo)
-		#echo Total Memory = $memtot 
-		#echo Available Memory = $memAva
-		#echo Active Memory = $memact
-		#echo $memtot - $memAva | bc -l
 		mempctAct=$(echo $memact / $memtot | bc -l)
 		memforty=$(echo $memtot \* 40 / 100 | bc -l)
 		if [ $(echo $mempctAct '<' $memforty | bc -l) ]; then
-			#echo "beat"
-			#echo "beat"
 			memAva=$(echo " $memtot * 60 / 100" | bc -l)
-			echo $memAva
+			#echo $memAva
 			usermem=$(echo "$memAva/$2*1000000" | bc -l)
-			#userline=$(cat /etc/security/limits.conf | grep -n $1) # | cut -f1 -d:)
-			#echo exit code = $?
 			roundedmem=$(printf "%.0f\n" $usermem)
-			#echo roundedmem = $roundedmem
 			userreplace=$(echo @$1 hard as $roundedmem)
-			#echo Usermem = $userreplace
-			#first, remove any lines changing the AS limit
-			#echo userline = $userline
-			#echo beat
 			userline=$(cat /etc/security/limits.conf | grep -n $1 | wc -l)
 			if [ $userline -eq 0 ]; then
 				echo "$userreplace" >> /etc/security/limits.conf
 			else
 				userline=$(cat /etc/security/limits.conf | grep -n $1)
 				userlineno=${userline%%:*}
-				#echo $? = question
-				#case for exit code 0, means user was found in the file
-				# therefore time to replace the line, not cat to the file
-				#echo $userline = user line
-				echo $userlineno = user line
+				#echo $userlineno = user line
 				linetoreplace=$(sudo sed "${userlineno}q;d" /etc/security/limits.conf)
-				#echo beat
-				echo linetoreplace = $linetoreplace
+				#echo linetoreplace = $linetoreplace
 				sudo sed -i "s/$linetoreplace/$userreplace/" /etc/security/limits.conf
-				
-				#echo "$userreplace" >> /etc/security/limits.conf
+				if [ "$linetoreplace" != "$userreplace" ]; then
+					echo "You will be logged out in 5 minutes" | write $1
+					echo "skill -kill -u $1" | at now + 5 minutes
+				fi
 			fi
 		fi
 		exit
 	else
-		echo "Cannot allot resources to user with limiting factor of $2"
+		#echo "Cannot allot resources to user with limiting factor of $2"
 		exit
 	fi
 else
-	echo "User is not found, abort execution"
+	#echo "User is not found, abort execution"
 	exit
 fi
